aa976b75fd14c42173e02fdf87defb86a5aeb231
Aanchal Rana <aanchalrana0106@gmail.com>
2024-10-24 20:57:38 +0530

    changes

--- Task-1/script.js
+++ Task-1/script.js
@@ -1,10 +1,10 @@
 const btn=document.querySelector(".btn")
 const container = document.querySelector(".container")
 
-btn.addEventListener("click",(e) =>
+btn.addEventListener("click",(e) =>  
 {
-    e.preventDefault()            //to stop the data to 
-   if(btn.innerText ==="Light")   //light is a string
+    e.preventDefault()           
+   if(btn.innerText ==="Light")  
    {
     container.style.backgroundColor ="black"
     btn.innerText="Dark";
