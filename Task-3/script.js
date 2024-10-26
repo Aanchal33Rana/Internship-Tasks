019e55cb1fc7422a8d51102bd8d76aeaae62cefe
Aanchal Rana <aanchalrana0106@gmail.com>
2024-10-26 19:49:59 +0530

    ..

--- Task-3/script.js
+++ Task-3/script.js
@@ -16,7 +16,16 @@ btn.addEventListener("click", (e)=>
    const input=document.querySelector("#url").value
    const url=`https://article-extractor-and-summarizer.p.rapidapi.com/summarize?url=${input}&lang=en&engine=2`;
 
-   fetch(url, options)
+   summarize.innerText = "Please wait article is summarazing"
+   
+   if (/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/.test(input))
+   {
+    //regular expressions
+    summarize.innerText ="Invalid URL ! please provide a correct URL"
+   }
+   else
+   {
+    fetch(url, options)
    .then(data=> data.json())
    .then(data=> 
     {
@@ -27,6 +36,7 @@ btn.addEventListener("click", (e)=>
     {
         console.log(error)
     })
+   }
 })
 
 /*fetch(url,options)
